1ce0ea07e0f74e0933a6a59f8e6f0ef47001a91c
Lutttfi <lutfiofficial149@gmail.com>
2025-04-12 23:03:35 +0700

    ubah

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = "I LOVE U".split("");
+    const titles = "Kiaa Cantikkk".split("");
     const titleElement = document.getElementById("title");
     let index = 0;
 
